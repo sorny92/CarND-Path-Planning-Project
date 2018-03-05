bd9775a6c9d8891d462e11b7a358ba129c25bcf4
Esteve Soria <esofabian@gmail.com>
2018-03-05 07:15:50 +0100

    Keep safetely the lane

--- src/main.cpp
+++ src/main.cpp
@@ -126,7 +126,7 @@ vector<double> getFrenet(double x, double y, double theta,
 }
 
 // Transform from Frenet s,d coordinates to Cartesian x,y
-pair<double, double> getXY(double s, double d, const vector<double> &maps_s,
+vector<double> getXY(double s, double d, const vector<double> &maps_s,
                            const vector<double> &maps_x,
                            const vector<double> &maps_y) {
   int prev_wp = -1;
@@ -150,7 +150,7 @@ pair<double, double> getXY(double s, double d, const vector<double> &maps_s,
   double x = seg_x + d * cos(perp_heading);
   double y = seg_y + d * sin(perp_heading);
 
-  return make_pair(x, y);
+  return {x, y};
 }
 
 int main() {
@@ -191,11 +191,12 @@ int main() {
     map_waypoints_dy.push_back(d_y);
   }
 
-  int lane = 6;
-  double ref_vel = 30;
+  int lane = 1;
+  double ref_vel = 0;
+  const double kMAX_SPEED = 49.5;
 
-  h.onMessage([&lane, &ref_vel, &map_waypoints_x, &map_waypoints_y, &map_waypoints_s,
-               &map_waypoints_dx,
+  h.onMessage([&lane, &ref_vel, &kMAX_SPEED, &map_waypoints_x, &map_waypoints_y,
+               &map_waypoints_s, &map_waypoints_dx,
                &map_waypoints_dy](uWS::WebSocket<uWS::SERVER> ws, char *data,
                                   size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -236,110 +237,148 @@ int main() {
           /***********************************************************************/
           /* START
           /***********************************************************************/
-          // TODO: define a path made up of (x,y) points that the car will visit
           int prev_size = previous_path_x.size();
+          int speed_car_ahead = kMAX_SPEED;
 
-          /* vector with points car is going to follow  */
-          vector<pair<double, double>> pts;
-          /* Where I'm now */
-          pair<double, double> ref(car_x, car_y);
-          /* Where I'm heading */
+          if(prev_size > 0) {
+            car_s = end_path_s;
+          }
+
+          bool too_close = false;
+
+          for(int i = 0; i <sensor_fusion.size(); i++) {
+            float d = sensor_fusion[i][6];
+            if(d < (2+4*lane+2) && d > (2+4*lane-2)) {
+              double vx = sensor_fusion[i][3];
+              double vy = sensor_fusion[i][4];
+              double check_speed = sqrt(vx*vx + vy*vy);
+              double check_car_s = sensor_fusion[i][5];
+
+              check_car_s+=((double)prev_size*.02*check_speed);
+              if((check_car_s > car_s) && ((check_car_s-car_s) < 30)) {
+                too_close = true;
+                speed_car_ahead = check_speed ;
+              }
+            }
+          }
+
+          if(too_close && ref_vel > speed_car_ahead) {
+            ref_vel -= .224;
+          }else if(ref_vel < kMAX_SPEED && ref_vel < speed_car_ahead) {
+            ref_vel += .224;
+          }
+
+
+
+
+
+
+          /** TRAJECTORY GENERATION */
+
+          vector<double> ptsx;
+          vector<double> ptsy;
+
+          double ref_x = car_x;
+          double ref_y = car_y;
           double ref_yaw = deg2rad(car_yaw);
 
-          /* In case there's no points, lets add were we are right now */
+          // Do I have have previous points
           if (prev_size < 2) {
-            pair<double, double> prev_car(ref.first - cos(car_yaw),
-                                          ref.second - sin(car_yaw));
-            cout << prev_car.first << prev_car.second << endl;
-            cout << ref.first << ref.second << endl;
-            pts.push_back(prev_car);
-            pts.push_back(ref);
+            // There are not too many...
+            double prev_car_x = car_x - cos(car_yaw);
+            double prev_car_y = car_y - sin(car_yaw);
+
+            ptsx.push_back(prev_car_x);
+            ptsx.push_back(car_x);
+
+            ptsy.push_back(prev_car_y);
+            ptsy.push_back(car_y);
           } else {
-            /* In case there is previous points on the vector,
-             * let's add to our path vector our last position and the one before
-             * that */
-            ref = make_pair(previous_path_x[prev_size - 1],
-                            previous_path_y[prev_size - 1]);
+            // Use the last two points.
+            ref_x = previous_path_x[prev_size - 1];
+            ref_y = previous_path_y[prev_size - 1];
 
-            pair<double, double> ref_prev(previous_path_x[prev_size - 2],
-                                          previous_path_y[prev_size - 2]);
-            ref_yaw =
-                atan2(ref.second - ref_prev.second, ref.first - ref_prev.first);
-            cout << ref_prev.first << ref_prev.second << endl;
-            cout << ref.first << ref.second << endl;
-            pts.push_back(ref_prev);
-            pts.push_back(ref);
+            double ref_x_prev = previous_path_x[prev_size - 2];
+            double ref_y_prev = previous_path_y[prev_size - 2];
+            ref_yaw = atan2(ref_y - ref_y_prev, ref_x - ref_x_prev);
+
+            ptsx.push_back(ref_x_prev);
+            ptsx.push_back(ref_x);
+
+            ptsy.push_back(ref_y_prev);
+            ptsy.push_back(ref_y);
           }
 
-          /* From Frenet coordinates get thre points in x,y coordinates ahead to
-           * draw a spline with them */
-          pair<double, double> next_wp0 =
-              getXY(car_s + 30, (2 + 4 * lane), map_waypoints_s,
-                    map_waypoints_x, map_waypoints_y);
-          pair<double, double> next_wp1 =
-              getXY(car_s + 60, (2 + 4 * lane), map_waypoints_s,
-                    map_waypoints_x, map_waypoints_y);
-          pair<double, double> next_wp2 =
-              getXY(car_s + 90, (2 + 4 * lane), map_waypoints_s,
-                    map_waypoints_x, map_waypoints_y);
+          // Setting up target points in the future.
+          vector<double> next_wp0 =
+              getXY(car_s + 30, 2 + 4 * lane, map_waypoints_s, map_waypoints_x,
+                    map_waypoints_y);
+          vector<double> next_wp1 =
+              getXY(car_s + 60, 2 + 4 * lane, map_waypoints_s, map_waypoints_x,
+                    map_waypoints_y);
+          vector<double> next_wp2 =
+              getXY(car_s + 90, 2 + 4 * lane, map_waypoints_s, map_waypoints_x,
+                    map_waypoints_y);
 
-          pts.push_back(next_wp0);
-          pts.push_back(next_wp1);
-          pts.push_back(next_wp2);
+          ptsx.push_back(next_wp0[0]);
+          ptsx.push_back(next_wp1[0]);
+          ptsx.push_back(next_wp2[0]);
 
-          /* ¿Set the points in the same direction? */
-          for (auto &pt : pts) {
-            pair<double, double> shift(pt.first - ref.first,
-                                       pt.second - ref.second);
+          ptsy.push_back(next_wp0[1]);
+          ptsy.push_back(next_wp1[1]);
+          ptsy.push_back(next_wp2[1]);
 
-            pt = make_pair(
-                shift.first * cos(-ref_yaw) - shift.second * sin(-ref_yaw),
-                shift.first * sin(-ref_yaw) + shift.second * cos(-ref_yaw));
+          // Making coordinates to local car coordinates.
+          for (int i = 0; i < ptsx.size(); i++) {
+            double shift_x = ptsx[i] - ref_x;
+            double shift_y = ptsy[i] - ref_y;
+
+            ptsx[i] = shift_x * cos(0 - ref_yaw) - shift_y * sin(0 - ref_yaw);
+            ptsy[i] = shift_x * sin(0 - ref_yaw) + shift_y * cos(0 - ref_yaw);
           }
 
-          /* Let's go to smooth the path */
+          // Create the spline.
           tk::spline s;
-          s.set_points(pts);
+          s.set_points(ptsx, ptsy);
 
+          // Output path points from previous path for continuity.
           vector<double> next_x_vals;
           vector<double> next_y_vals;
-
-          /* We need to cover the path we said before then add new points */
-          for (auto i = 0; i < prev_size ; i++) {
+          for (int i = 0; i < prev_size; i++) {
             next_x_vals.push_back(previous_path_x[i]);
             next_y_vals.push_back(previous_path_y[i]);
           }
+
+          // Calculate distance y position on 30 m ahead.
           double target_x = 30.0;
           double target_y = s(target_x);
-          double target_dist =
-              sqrt((target_x * target_x) + (target_y * target_y));
+          double target_dist = sqrt(target_x * target_x + target_y * target_y);
 
           double x_add_on = 0;
 
-          for (auto i = 1; i <= 50 - prev_size; i++) {
-            /* Number of points we need to get the target based of ref_vel */
-            double N = target_dist / (.02 * ref_vel / 2.24);
-            double point_x = x_add_on + (target_x)/N;
-            double point_y = s(point_x);
+          for (int i = 1; i < 50 - prev_size; i++) {
+            double N = target_dist / (0.02 * ref_vel / 2.24);
+            double x_point = x_add_on + target_x / N;
+            double y_point = s(x_point);
 
-            x_add_on = point_x;
+            x_add_on = x_point;
 
-            double x_ref = point_x;
-            double y_ref = point_y;
+            double x_ref = x_point;
+            double y_ref = y_point;
 
-            /* ¿Rotate to normal? */
-            point_x = (x_ref * cos(ref_yaw) - y_ref * sin(ref_yaw));
-            point_y = (x_ref * sin(ref_yaw) + y_ref * cos(ref_yaw));
+            x_point = x_ref * cos(ref_yaw) - y_ref * sin(ref_yaw);
+            y_point = x_ref * sin(ref_yaw) + y_ref * cos(ref_yaw);
 
-            point_x += x_ref+ ref.first;
-            point_y += y_ref+ ref.second;
+            x_point += ref_x;
+            y_point += ref_y;
 
-            next_x_vals.push_back(point_x);
-            next_y_vals.push_back(point_y);
+            next_x_vals.push_back(x_point);
+            next_y_vals.push_back(y_point);
           }
 
-          for (auto i = 0; i < next_x_vals.size(); i++){
-            cout << i << "-\t " << next_x_vals[i] << " " << next_y_vals[i] << endl;
+          for (auto i = 0; i < next_x_vals.size(); i++) {
+            cout << i << "-\t " << next_x_vals[i] << " " << next_y_vals[i]
+                 << endl;
           }
           // sequentially every .02 seconds
           /***********************************************************************/
